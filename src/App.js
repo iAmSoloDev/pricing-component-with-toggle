24b0f26e828cfc25e7c71fcc2053b361350939e7
iAmSoloDev <107878651+iAmSoloDev@users.noreply.github.com>
2023-06-17 02:53:38 +0000

    add background images and delete some unused files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import Main from './Main';
+import bgBottom from './bg-bottom.svg'
+import bgTop from './bg-top.svg'
 
 function App() {
   return (
     <>
+      <div className='bg-top'>
+        <img src={bgTop} alt='' className='bg-width' />
+      </div>
       <Main />
+      <div className='bg-bottom'>
+        <img src={bgBottom} alt='' className='bg-width' />
+      </div>
     </>
   );
 }
